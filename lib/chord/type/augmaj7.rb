db7938908d990141fa4fcc891e43c38d8b034b27
Andreas Wachowski <andreas.wachowski@gmail.com>
2014-03-23 15:18:42 +0100

    Added chord types: aug7 and augmaj7

---
+++ lib/chord/type/augmaj7.rb
@@ -0,0 +1,23 @@
+class Chord; end;
+class Chord::Type; end;
+class Chord::Type::AugMaj7 < Chord::Type
+  def self.in_chord_symbol
+    "+maj7"
+  end
+
+  def self.norm_interval_structure
+    [ "1", "3", "#5", "7" ]
+  end
+
+  def self.anki_filename
+    "augmaj7"
+  end
+
+  def self.anki_tag
+    "augmaj7"
+  end
+
+  def self.html_symbol
+    "+maj7"
+  end
+end
