cf43144087771312045d00ea732b2c4223255551
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-02-11 19:22:02 +0100

    style: rubocop Naming/VariableName

--- test/tc_anki_generator.rb
+++ test/tc_anki_generator.rb
@@ -41,10 +41,10 @@ class TestAnkiGenerator < Test::Unit::TestCase
     # Ensure LaTeXPianoChordWriter is called with correct file name, and
     # Anki record is generated
     c = Chord.new('As', :maj7, :third)
-    ankiGenerator = AnkiGenerator.new(@dirname, @ankifile, @force)
+    anki_generator = AnkiGenerator.new(@dirname, @ankifile, @force)
 
     LaTeXPianoChordWriter.any_instance.stubs(:generate_png).with(AnkiChordWriter.new(c).filename).returns(true)
-    ankiGenerator.generate(['As'], [:maj7], [:third])
+    anki_generator.generate(['As'], [:maj7], [:third])
     LaTeXPianoChordWriter.any_instance.unstub(:generate_png)
 
     # Make sure one anki record is written to the file
@@ -56,10 +56,10 @@ class TestAnkiGenerator < Test::Unit::TestCase
     # Generation of all inversions for a seventh/4-tone-chord
     Chord.new('As', :maj7, :third)
 
-    ankiGenerator = AnkiGenerator.new(@dirname, @ankifile, @force)
+    anki_generator = AnkiGenerator.new(@dirname, @ankifile, @force)
 
     LaTeXPianoChordWriter.any_instance.stubs(:generate_png).returns(true)
-    ankiGenerator.generate(['As'], [:maj7], Chord::Type.all_inversions)
+    anki_generator.generate(['As'], [:maj7], Chord::Type.all_inversions)
     LaTeXPianoChordWriter.any_instance.unstub(:generate_png)
 
     count = %x{wc -l #{@ankifile}}.split.first.to_i
@@ -71,7 +71,7 @@ class TestAnkiGenerator < Test::Unit::TestCase
     # with a seventh/4-tone-chord
 
     LaTeXPianoChordWriter.any_instance.stubs(:generate_png).returns(true)
-    ankiGenerator.generate(['As'], [:aug, :maj7], Chord::Type.all_inversions)
+    anki_generator.generate(['As'], [:aug, :maj7], Chord::Type.all_inversions)
     LaTeXPianoChordWriter.any_instance.unstub(:generate_png)
 
     count = %x{wc -l #{@ankifile}}.split.first.to_i
